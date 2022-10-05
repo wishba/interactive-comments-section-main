ff2aa49a8a5ec2eaeb6393cba003869d64bbf03e
wishba <wisnubayu34@gmail.com>
2022-10-05 22:08:20 +0700

    render data 0

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 fetch('data.json')
   .then(res => res.json())
-  .then(data => {
-    document.getElementById('comments').innerHTML = JSON.stringify(data)
-  })
+  .then(data => appendData(data))
+
+function appendData(data) {
+  let comments = document.getElementById('comments')
+  comments.innerHTML = `
+    <p>${data.comments[0].user.username}</p>
+    <p>${data.comments[0].createdAt}</p>
+    <p>${data.comments[0].content}</p>
+    <p>${data.comments[0].score}</p>
+  `;
+}
