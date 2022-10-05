9315c0d809187e6910500d6ee2e36336d59e18c4
wishba <wisnubayu34@gmail.com>
2022-10-05 22:41:26 +0700

    profile picture

--- script.js
+++ script.js
@@ -5,6 +5,11 @@ fetch('data.json')
 function appendData(data) {
   let comments = document.getElementById('comments')
   comments.innerHTML = `
+    <picture>
+      <source srcset="${data.comments[0].user.image.webp}" type="image/webp">
+      <source srcset="${data.comments[0].user.image.png}" type="image/png"> 
+      <img src="${data.comments[0].user.image.png}" alt="profile picture">
+    </picture>
     <p>${data.comments[0].user.username}</p>
     <p>${data.comments[0].createdAt}</p>
     <p>${data.comments[0].content}</p>
