69bbf3606aeb730f1b98f351dd9d1cbe76545171
wishba <wisnubayu34@gmail.com>
2022-10-06 13:30:24 +0700

    log empty or replies if any

--- script.js
+++ script.js
@@ -4,9 +4,10 @@ fetch('data.json')
 
 function appendData(data) {
   for (const iterator of data.comments) {
-    let comments = document.getElementById('comments')
-    let notificationContainer = document.createElement("div")
-    notificationContainer.innerHTML = `
+    let commentsContainer = document.getElementById('comments')
+    let comment = document.createElement("div")
+    let commentChild = 'tes'
+    comment.innerHTML = `
       <picture>
       <source srcset="${iterator.user.image.webp}" type="image/webp">
       <source srcset="${iterator.user.image.png}" type="image/png"> 
@@ -16,8 +17,21 @@ function appendData(data) {
       <p>${iterator.createdAt}</p>
       <p>${iterator.content}</p>
       <p>${iterator.score}</p>
+      <p>${commentChild}</p>
     `;
-    comments.appendChild(notificationContainer);
+    commentsContainer.appendChild(comment);
   }
-  console.log(data.comments[1].replies[0].replyingTo);
+
+  for (let index = 0; index < data.comments.length; index++) {
+    const element = data.comments[index];
+    console.log(element.replies.length);
+    if (element.replies.length == 0) {
+      console.log('empty');
+    } else {
+      for (let i = 0; i < element.replies.length; i++) {
+        const e = element.replies[i];
+        console.log(e);
+      }
+    }
+  }
 }
