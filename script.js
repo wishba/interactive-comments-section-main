9a46322ed95246f90f075f9df63da98a2596dd65
wishba <wisnubayu34@gmail.com>
2022-10-06 13:53:04 +0700

    log replyingTo, empty if empty

--- script.js
+++ script.js
@@ -3,10 +3,22 @@ fetch('data.json')
   .then(data => appendData(data))
 
 function appendData(data) {
+  for (let index = 0; index < data.comments.length; index++) {
+    const element = data.comments[index]
+    if (element.replies.length > 0) {
+      for (let i = 0; i < element.replies.length; i++) {
+        const e = element.replies[i]
+        console.log(e.replyingTo)
+        nameReply = e.replyingTo
+      }
+    } else {
+      console.log('empty')
+    }
+  }
+
   for (const iterator of data.comments) {
     let commentsContainer = document.getElementById('comments')
     let comment = document.createElement("div")
-    let commentChild = 'tes'
     comment.innerHTML = `
       <picture>
       <source srcset="${iterator.user.image.webp}" type="image/webp">
@@ -17,21 +29,7 @@ function appendData(data) {
       <p>${iterator.createdAt}</p>
       <p>${iterator.content}</p>
       <p>${iterator.score}</p>
-      <p>${commentChild}</p>
-    `;
-    commentsContainer.appendChild(comment);
-  }
-
-  for (let index = 0; index < data.comments.length; index++) {
-    const element = data.comments[index];
-    console.log(element.replies.length);
-    if (element.replies.length == 0) {
-      console.log('empty');
-    } else {
-      for (let i = 0; i < element.replies.length; i++) {
-        const e = element.replies[i];
-        console.log(e);
-      }
-    }
+    `
+    commentsContainer.appendChild(comment)
   }
 }
