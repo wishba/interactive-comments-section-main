660ca4930b897f86ffc6bed1cfb1ccd9d4a9b1c0
wishba <wisnubayu34@gmail.com>
2022-10-06 15:57:08 +0700

    render 'empty' or 'reply' with for-of loop

--- script.js
+++ script.js
@@ -3,44 +3,73 @@ fetch('data.json')
   .then(data => appendData(data))
 
 function appendData(data) {
-  let commentsReplyContainer = document.getElementById('commentsReply')
-  for (let index = 0; index < data.comments.length; index++) {
-    const element = data.comments[index]
-    console.log(element.replies.length);
+  let commentsContainer = document.getElementById('comments')
 
-    if (element.replies.length == 0) {
-      console.log('empty');
-
-      let commentReply = document.createElement("div")
+  for (const dataComments of data.comments) {
+    if (dataComments.replies.length == 0) {
+      let commentReply = document.createElement('div')
       commentReply.innerHTML = 'empty'
-      commentsReplyContainer.appendChild(commentReply)
+      commentsContainer.appendChild(commentReply)
     } else {
-
-      for (let i = 0; i < element.replies.length; i++) {
-        const e = element.replies[i];
-        console.log('reply name');
-
-        let commentReply = document.createElement("div")
-        commentReply.innerHTML = 'reply name'
-        commentsReplyContainer.appendChild(commentReply)
+      for (const dataReplies of dataComments.replies) {
+        console.log('reply');
+        let commentReply = document.createElement('div')
+        commentReply.innerHTML = 'reply'
+        commentsContainer.appendChild(commentReply)
       }
     }
+
+
+    // console.log(comments.replies.length);
+    // if (comments.replies.length == 0) {
+    //   console.log('empty');
+    //   commentReply.innerHTML = 'empty'
+    // } else {
+    //   for (const replies of comments.replies) {
+    //     console.log('reply');
+    //     commentReply.innerHTML = 'reply'
+    //   }
+    // }
+    // commentsContainer.appendChild(commentReply)
   }
 
-  let commentsContainer = document.getElementById('comments')
-  for (const iterator of data.comments) {
-    let comment = document.createElement("div")
-    comment.innerHTML = `
-      <picture>
-      <source srcset="${iterator.user.image.webp}" type="image/webp">
-      <source srcset="${iterator.user.image.png}" type="image/png"> 
-        <img src="${iterator.user.image.png}" alt="profile picture">
-      </picture>
-      <p>${iterator.user.username}</p>
-      <p>${iterator.createdAt}</p>
-      <p>${iterator.content}</p>
-      <p>${iterator.score}</p>
-    `
-    commentsContainer.appendChild(comment)
-  }
+  // for (let index = 0; index < data.comments.length; index++) {
+  //   const element = data.comments[index]
+  //   // console.log(element.replies.length);
+
+  //   if (element.replies.length == 0) {
+  //     // console.log('empty');
+
+  //     let commentReply = document.createElement("div")
+  //     commentReply.innerHTML = 'empty'
+  //     commentsReplyContainer.appendChild(commentReply)
+  //   } else {
+
+  //     for (let i = 0; i < element.replies.length; i++) {
+  //       const e = element.replies[i];
+  //       // console.log('reply name');
+
+  //       let commentReply = document.createElement("div")
+  //       commentReply.innerHTML = 'reply name'
+  //       commentsReplyContainer.appendChild(commentReply)
+  //     }
+  //   }
+  // }
+
+  // let commentsContainer = document.getElementById('comments')
+  // for (const iterator of data.comments) {
+  //   let comment = document.createElement("div")
+  //   comment.innerHTML = `
+  //     <picture>
+  //     <source srcset="${iterator.user.image.webp}" type="image/webp">
+  //     <source srcset="${iterator.user.image.png}" type="image/png"> 
+  //       <img src="${iterator.user.image.png}" alt="profile picture">
+  //     </picture>
+  //     <p>${iterator.user.username}</p>
+  //     <p>${iterator.createdAt}</p>
+  //     <p>${iterator.content}</p>
+  //     <p>${iterator.score}</p>
+  //   `
+  //   commentsContainer.appendChild(comment)
+  // }
 }
