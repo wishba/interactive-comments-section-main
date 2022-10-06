b8d4278c15b019ba739968578846ed91a8136818
wishba <wisnubayu34@gmail.com>
2022-10-06 12:47:49 +0700

    log replyingTo

--- script.js
+++ script.js
@@ -4,20 +4,20 @@ fetch('data.json')
 
 function appendData(data) {
   for (const iterator of data.comments) {
-    console.log(iterator);
     let comments = document.getElementById('comments')
-    let div = document.createElement("div")
-    div.innerHTML = `
-        <picture>
-          <source srcset="${iterator.user.image.webp}" type="image/webp">
-          <source srcset="${iterator.user.image.png}" type="image/png"> 
-          <img src="${iterator.user.image.png}" alt="profile picture">
-        </picture>
-        <p>${iterator.user.username}</p>
-        <p>${iterator.createdAt}</p>
-        <p>${iterator.content}</p>
-        <p>${iterator.score}</p>
-      `;
-    comments.appendChild(div);
+    let notificationContainer = document.createElement("div")
+    notificationContainer.innerHTML = `
+      <picture>
+      <source srcset="${iterator.user.image.webp}" type="image/webp">
+      <source srcset="${iterator.user.image.png}" type="image/png"> 
+        <img src="${iterator.user.image.png}" alt="profile picture">
+      </picture>
+      <p>${iterator.user.username}</p>
+      <p>${iterator.createdAt}</p>
+      <p>${iterator.content}</p>
+      <p>${iterator.score}</p>
+    `;
+    comments.appendChild(notificationContainer);
   }
+  console.log(data.comments[1].replies[0].replyingTo);
 }
