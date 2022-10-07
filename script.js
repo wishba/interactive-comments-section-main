1fce14fd9cc9bd6a89b33c42c751ec4a33ac6169
wishba <wisnubayu34@gmail.com>
2022-10-07 20:43:41 +0700

    render count time

--- script.js
+++ script.js
@@ -15,11 +15,36 @@ let shortedKeyArray = keyArray.sort().reverse()
 function appendData(data) {
   let commentsContainer = document.getElementById('comments')
 
-  // loop through shortedKeyArray, render sorted localStorage
+
+
+  // render sorted localStorage
   for (const iterator of shortedKeyArray) {
     let getStorage = localStorage.getItem(iterator)
     let StorageObject = JSON.parse(getStorage)
     let localStorageContainer = document.createElement('div')
+
+    // count time
+    let objectDate = new Date();
+    let yearNow = objectDate.getFullYear();
+    let monthNow = objectDate.getMonth();
+    let dayNow = objectDate.getDate();
+    let hourNow = objectDate.getHours();
+    let minuteNow = objectDate.getMinutes();
+    let commentTime = ''
+    if (yearNow - StorageObject.createdAt.year > 0) {
+      commentTime = yearNow - StorageObject.createdAt.year + ' year ago'
+    } else if (monthNow - StorageObject.createdAt.month > 0) {
+      commentTime = monthNow - StorageObject.createdAt.month + ' month ago'
+    } else if (dayNow - StorageObject.createdAt.day > 0) {
+      commentTime = dayNow - StorageObject.createdAt.day + ' day ago'
+    } else if (hourNow - StorageObject.createdAt.hour > 0) {
+      commentTime = hourNow - StorageObject.createdAt.hour + ' hour ago'
+    } else if (minuteNow - StorageObject.createdAt.minute > 0) {
+      commentTime = minuteNow - StorageObject.createdAt.minute + ' minute ago'
+    } else {
+      commentTime = 'just now'
+    }
+
     localStorageContainer.innerHTML = `
       <picture>
         <source type="image/webp" srcset="${StorageObject.user.image.webp}">
@@ -27,13 +52,14 @@ function appendData(data) {
         <img src="${StorageObject.user.image.png}" alt="user profile picture">
       </picture>
       <p>${StorageObject.user.username}</p>
+      <p>${commentTime}</p>
       <p>${StorageObject.content}</p>
       <p>${StorageObject.score}</p>
     `
     commentsContainer.appendChild(localStorageContainer)
   }
 
-  // retrieving data from json
+  // render json
   for (const dataComments of data.comments) {
 
     // comments
@@ -124,33 +150,3 @@ async function handleSubmit() {
   const commentJSON = JSON.stringify(commentObjIn)
   localStorage.setItem(timeStamp, commentJSON)
 }
-
-
-
-// let objectDate = new Date();
-
-// let minute = objectDate.getMinutes();
-// let hour = objectDate.getHours();
-// let day = objectDate.getDate();
-// let month = objectDate.getMonth();
-// let year = objectDate.getFullYear();
-
-// let storageMinute = 57
-// let storageHour = 18
-// let storageDay = 7
-// let storageMonth = 10
-// let storageYear = 2022
-
-// if (year - storageYear > 0) {
-//   console.log(year - storageYear + ' year ago');
-// } else if (month - storageMonth + 1 > 0) {
-//   console.log(month - storageMonth + 1 + ' month ago');
-// } else if (day - storageDay > 0) {
-//   console.log(day - storageDay + ' day ago');
-// } else if (hour - storageHour > 0) {
-//   console.log(hour - storageHour + ' hour ago');
-// } else if (minute - storageMinute > 0) {
-//   console.log(minute - storageMinute + ' minute ago');
-// } else {
-//   console.log('just now');
-// }
