4ac9b8411e0f5b10c07df410bcdbc49a84b9f480
wishba <wisnubayu34@gmail.com>
2022-10-07 09:19:20 +0700

    render sorted localStorage

--- script.js
+++ script.js
@@ -2,37 +2,25 @@ fetch('data.json')
   .then(res => res.json())
   .then(data => appendData(data))
 
-// make array of localStorage key
+// make sorted array of localStorage key
 let keyArray = []
 for (let index = 0; index < localStorage.length; index++) {
   const storageKey = localStorage.key(index)
   keyArray.push(storageKey)
 }
-console.log(keyArray);
-
-// sort the array of localStorage key
 let shortedKeyArray = keyArray.sort().reverse()
-console.log(shortedKeyArray);
-
-// loop through shortedKeyArray, return sorted localStorage
-console.log(localStorage);
-for (const iterator of shortedKeyArray) {
-  console.log(iterator);
-  console.log(localStorage.getItem(iterator));
-}
 
 function appendData(data) {
   let commentsContainer = document.getElementById('comments')
 
-  // retrieving data from localStorage
-  for (let index = 0; index < localStorage.length; index++) {
+  // loop through shortedKeyArray, render sorted localStorage
+  for (const iterator of shortedKeyArray) {
+    let getStorage = localStorage.getItem(iterator)
+    let StorageObject = JSON.parse(getStorage)
     let localStorageContainer = document.createElement('div')
-    const storageKey = localStorage.key(index)
-    let storageValue = localStorage.getItem(storageKey)
-    let storageValueObj = JSON.parse(storageValue)
     localStorageContainer.innerHTML = `
-      <p>key: ${storageKey}</p>
-      <p>comment: ${storageValueObj.comment}</p>
+      <p>${iterator}</p>
+      <p>${StorageObject.content}</p>
     `
     commentsContainer.appendChild(localStorageContainer)
   }
