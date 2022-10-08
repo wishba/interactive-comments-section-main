45fe31e27fb0dbc8fc74dbbf60247e040643ef1a
wishba <wisnubayu34@gmail.com>
2022-10-08 12:03:43 +0700

    up CommentReplyParent

---
+++ src/component/CommentReplyParent.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import CommentReply from './CommentReply'
+
+function CommentReplyParent() {
+  return (
+    <>
+      <CommentReply index='0' />
+      <CommentReply index='1' />
+    </>
+  )
+}
+
+export default CommentReplyParent
