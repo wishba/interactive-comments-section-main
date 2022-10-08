17364c4bd56cdedabb5a8560b6a6ce3705d923ce
wishba <wisnubayu34@gmail.com>
2022-10-09 01:00:16 +0700

    render data detail

--- src/component/CommentReply.jsx
+++ src/component/CommentReply.jsx
@@ -5,7 +5,24 @@ function CommentReply(props) {
   return (
     <>
       {data.comments[props.commentIndex].replies.map(reply => (
-        <p key={reply.id}>reply id: {reply.id}</p>
+        <div key={reply.id}>
+          {console.log(reply.user.image.png)}
+
+          <p>{reply.score}</p>
+
+          <picture>
+            <source type="image/webp" srcSet={reply.user.image.webp} />
+            <source type="image/png" srcSet={reply.user.image.png} />
+            <img src={reply.user.image.png} alt="user profile picture" />
+          </picture>
+          <p>{reply.user.username}</p>
+          <p>{reply.createdAt}</p>
+
+          <p>Reply</p>
+
+          <p>@{reply.replyingTo}</p>
+          <p>{reply.content}</p>
+        </div>
       ))}
     </>
   )
