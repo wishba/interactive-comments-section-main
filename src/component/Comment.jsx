38cce245ff4e56c32810c592b31471caf890ef86
wishba <wisnubayu34@gmail.com>
2022-10-08 12:03:06 +0700

    return CommentReplyParent

--- src/component/Comment.jsx
+++ src/component/Comment.jsx
@@ -1,23 +1,11 @@
 import React from 'react'
-import data from './data/data.json'
+import CommentReplyParent from './CommentReplyParent'
 
 function Comment() {
   return (
-    <div>
-      {data.comments.map(comment => (
-        <div key={comment.id}>
-          {console.log(comment.replies)}
-          <p>{comment.id}</p>
-
-          {comment.replies.map(reply => (
-            <div key={reply.id}>
-              {console.log(reply.id)}
-              <p>{reply.id}</p>
-            </div>
-          ))}
-        </div>
-      ))}
-    </div>
+    <>
+      <CommentReplyParent />
+    </>
   )
 }
 
