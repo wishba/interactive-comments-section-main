ebbe3dac67be03a7bb6635af2acc239fa7f60d56
wishba <wisnubayu34@gmail.com>
2022-10-08 19:37:08 +0700

    delete space

--- src/component/CommentReply.jsx
+++ src/component/CommentReply.jsx
@@ -4,11 +4,9 @@ import data from './data/data.json'
 function CommentReply(props) {
   return (
     <>
-      {
-        data.comments[props.commentIndex].replies.map(reply => (
-          <p key={reply.id}>reply id: {reply.id}</p>
-        ))
-      }
+      {data.comments[props.commentIndex].replies.map(reply => (
+        <p key={reply.id}>reply id: {reply.id}</p>
+      ))}
     </>
   )
 }
