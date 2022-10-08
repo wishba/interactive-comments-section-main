4d52c474c6ae8b9aafc643e41a3a4014c35cfaa2
wishba <wisnubayu34@gmail.com>
2022-10-09 01:22:30 +0700

    render data detail

--- src/component/CommentReplyParent.jsx
+++ src/component/CommentReplyParent.jsx
@@ -8,7 +8,19 @@ function CommentReplyParent() {
       {data.comments.map((comment, index) => (
         <div key={data.comments[index].id}>
           <div>
-            <p>index: {index}</p>
+            <p>{comment.score}</p>
+
+            <picture>
+              <source type="image/webp" srcSet={comment.user.image.webp} />
+              <source type="image/png" srcSet={comment.user.image.png} />
+              <img src={comment.user.image.png} alt="user profile picture" />
+            </picture>
+            <p>{comment.user.username}</p>
+            <p>{comment.createdAt}</p>
+
+            <p>Reply</p>
+
+            <p>{comment.content}</p>
           </div>
           <CommentReply commentIndex={index} />
         </div>
