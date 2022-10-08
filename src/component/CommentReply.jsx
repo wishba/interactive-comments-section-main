fdca4a59cb603632b450eec5f0211afe8f8b31db
wishba <wisnubayu34@gmail.com>
2022-10-09 02:18:44 +0700

    red bar in reply

--- src/component/CommentReply.jsx
+++ src/component/CommentReply.jsx
@@ -5,25 +5,28 @@ function CommentReply(props) {
   return (
     <>
       {data.comments[props.commentIndex].replies.map(reply => (
-        <div key={reply.id} className='comment__container comment__container--reply'>
-          <p className='comment__score'>{reply.score}</p>
+        <div className="comment__reply-grid-container">
+          <div className="comment__reply-grid-line"></div>
+          <div key={reply.id} className='comment__container comment__container--reply'>
+            <p className='comment__score'>{reply.score}</p>
 
-          <div className='comment__head'>
-            <picture>
-              <source type="image/webp" srcSet={reply.user.image.webp} />
-              <source type="image/png" srcSet={reply.user.image.png} />
-              <img src={reply.user.image.png} alt="user profile picture" />
-            </picture>
-            <p>{reply.user.username}</p>
-            <p>{reply.createdAt}</p>
+            <div className='comment__head'>
+              <picture>
+                <source type="image/webp" srcSet={reply.user.image.webp} />
+                <source type="image/png" srcSet={reply.user.image.png} />
+                <img src={reply.user.image.png} alt="user profile picture" />
+              </picture>
+              <p>{reply.user.username}</p>
+              <p>{reply.createdAt}</p>
+            </div>
+
+            <p className='comment__reply'>Reply</p>
+
+            <p className='comment__content'>
+              <span>@{reply.replyingTo}</span>
+              {reply.content}
+            </p>
           </div>
-
-          <p className='comment__reply'>Reply</p>
-
-          <p className='comment__content'>
-            <span>@{reply.replyingTo}</span>
-            {reply.content}
-          </p>
         </div>
       ))}
     </>
