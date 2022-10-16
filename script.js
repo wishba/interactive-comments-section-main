8b8d955195a95053e60c73e17a33288aa202afa7
wishba <wisnubayu34@gmail.com>
2022-10-16 10:37:04 +0700

    render id

--- script.js
+++ script.js
@@ -3,13 +3,21 @@ fetch('data.json')
   .then((data) => appendData(data))
 
 function appendData(data) {
+  const comments = document.getElementById('comments')
+
   const comment = data.comments
   for (const commentList of comment) {
-    console.log(commentList);
+    console.log(commentList)
+    let div = document.createElement('div')
+    div.innerHTML = commentList.id
+    comments.appendChild(div)
 
     const reply = commentList.replies
     for (const replyList of reply) {
-      console.log(replyList);
+      console.log(replyList)
+      let div = document.createElement('div')
+      div.innerHTML = replyList.id
+      comments.appendChild(div)
     }
   }
 }
