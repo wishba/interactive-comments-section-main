45ab44da9133149d16e5733af5736f8d627c07bf
wishba <wisnubayu34@gmail.com>
2022-10-16 13:56:06 +0700

    edit content

--- script.js
+++ script.js
@@ -49,7 +49,7 @@ function appendData(data) {
 // store data to local storage
 const storeObject = {
   id: 'test id',
-  content: 'test content'
+  content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
 };
 const storageJSON = JSON.stringify(storeObject);
 localStorage.setItem("testKey", storageJSON);
