91e916e86f992ae13470b3a38418e8f9e4aa0c23
wishba <wisnubayu34@gmail.com>
2022-10-16 11:49:39 +0700

    render all data

--- script.js
+++ script.js
@@ -2,30 +2,47 @@ fetch('data.json')
   .then((response) => response.json())
   .then((data) => appendData(data))
 
-function appendData(data) {
-  const comments = document.getElementById('comments')
+const renderedComment = document.getElementById('comments')
 
-  const comment = data.comments
-  for (const commentList of comment) {
-    console.log(commentList)
+function appendData(data) {
+  const dataComments = data.comments
+  for (const comment of dataComments) {
+    console.log(comment)
     let div = document.createElement('div')
     div.innerHTML = `
-      <p>${commentList.id}</p>
-      <p>${commentList.content}</p>
+      <p>${comment.score}</p>
+      <picture>
+        <source srcset="${comment.user.image.webp}" type="image/webp">
+        <img src="${comment.user.image.png}" alt="profile picture">
+      </picture>
+      <p>${comment.user.username}</p>
+      <p>${comment.createdAt}</p>
+      <p>Reply</p>
+      <p>${comment.content}</p>
       <hr>
     `
-    comments.appendChild(div)
+    renderedComment.appendChild(div)
 
-    const reply = commentList.replies
-    for (const replyList of reply) {
-      console.log(replyList)
+    const dataReply = comment.replies
+    for (const reply of dataReply) {
+      console.log(reply)
       let div = document.createElement('div')
       div.innerHTML = `
-        <p>${replyList.id}</p>
-        <p>${replyList.content}</p>
+        <p>${reply.score}</p>
+        <picture>
+          <source srcset="${comment.user.image.webp}" type="image/webp">
+          <img src="${comment.user.image.png}" alt="profile picture">
+        </picture>
+        <p>${reply.user.username}</p>
+        <p>${reply.createdAt}</p>
+        <p>Reply</p>
+        <p>
+          <span>@${reply.replyingTo}</span>
+          ${reply.content}
+        </p>
         <hr>
       `
-      comments.appendChild(div)
+      renderedComment.appendChild(div)
     }
   }
 }
