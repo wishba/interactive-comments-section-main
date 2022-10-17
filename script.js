0f47095a8339c2ebe1d4de9c52d884c70947548a
wishba <wisnubayu34@gmail.com>
2022-10-17 10:20:58 +0700

    separate sortKey function

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ fetch('data.json')
   .then((response) => response.json())
   .then((data) => {
     jsonData(data)
-    storageData(data)
+    storageData()
     formData(data)
     submitData(data)
   })
@@ -53,42 +53,23 @@ function jsonData(data) {
   }
 }
 
-// sorted array of localStorage key
-let keyArray = []
-for (let index = 0; index < localStorage.length; index++) {
-  const storageKey = localStorage.key(index)
-  keyArray.push(storageKey)
+function sortKey() {
+  let keyArray = []
+  for (let index = 0; index < localStorage.length; index++) {
+    const storageKey = localStorage.key(index)
+    keyArray.push(storageKey)
+  }
+  let shortedKeyArray = keyArray.sort().reverse()
+  return shortedKeyArray
 }
-let shortedKeyArray = keyArray.sort().reverse()
 
 // render from local storage
-function storageData(data) {
-  // const imagePNG = data.currentUser.image.png
-  // const imageWebp = data.currentUser.image.webp
-  // const name = data.currentUser.username
-
-  // store data to local storage
-  // const storeObject = {
-  //   id: 'test id',
-  //   content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-  //   createdAt: 'some time ago',
-  //   score: 0,
-  //   user: {
-  //     image: {
-  //       png: imagePNG,
-  //       webp: imageWebp
-  //     },
-  //     username: name
-  //   },
-  //   replies: []
-  // };
-  // const storageJSON = JSON.stringify(storeObject);
-  // localStorage.setItem("testKey", storageJSON);
+function storageData() {
 
   const storageCommentContainer = document.createElement("div");
 
   // retrieving data from local storage
-  for (const storageKey of shortedKeyArray) {
+  for (const storageKey of sortKey()) {
     let storageString = localStorage.getItem(storageKey);
     let storageValue = JSON.parse(storageString);
 
@@ -122,18 +103,12 @@ function formData(data) {
   `
 }
 
-// timestamp
-// let timeStamp = Date.now()
-// console.log(timeStamp);
-
 // form submit data
 function submitData(data) {
   document.getElementById('form').onsubmit = function (e) {
     e.preventDefault()
 
-
     let timeStamp = Date.now()
-    // let key = timeStamp
 
     // store data to local storage
     let inputContent = document.getElementById('inputContent').value
@@ -156,16 +131,9 @@ function submitData(data) {
     };
     const formJson = JSON.stringify(formInput);
     localStorage.setItem(timeStamp, formJson);
-    // localStorage.setItem(key, formJson);
-    // localStorage.setItem("testJSON", formJson);
 
-    // Retrieving data:
-    // console.log(timeStamp);
     let getData = localStorage.getItem(timeStamp);
-    // let getData = localStorage.getItem(key);
-    // let getData = localStorage.getItem("testJSON");
     let objectData = JSON.parse(getData);
-    // console.log(objectData);
 
     // create element
     const storedComment = document.createElement("div");
