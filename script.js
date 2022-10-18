5c08ca5500c39b2d1cdc82b692558c43642341c9
wishba <wisnubayu34@gmail.com>
2022-10-18 22:17:58 +0700

    loop

--- script.js
+++ script.js
@@ -1,64 +1,68 @@
 fetch('data.json')
   .then((response) => response.json())
   .then((data) => {
-    renderComments(data)
+    appendData(data)
   })
 
 const commentContainer = document.getElementById('commentContainer')
 
-function renderComments(data) {
-  const comment = document.createElement('div')
+function appendData(data) {
+  for (const dataComment of data.comments) {
+    const comment = document.createElement('div')
 
-  let getStorage = localStorage.getItem("testJSON");
-  let storage = JSON.parse(getStorage);
+    const key = 'key' + dataComment.id
 
-  const commentContent = document.createElement('p')
-  if (storage === null) {
-    commentContent.innerText = data.comments[0].content
-  } else {
-    commentContent.innerText = storage.content
-  }
-  comment.appendChild(commentContent)
+    let getStorage = localStorage.getItem(key)
+    let storage = JSON.parse(getStorage)
 
-  const replyToggle = document.createElement('a')
-  replyToggle.setAttribute('href', 'javascript:void(0)')
-  replyToggle.innerText = 'Reply'
-  comment.appendChild(replyToggle)
+    const commentContent = document.createElement('p')
+    if (storage === null) {
+      commentContent.innerText = dataComment.content
+    } else {
+      commentContent.innerText = storage.content
+    }
+    comment.appendChild(commentContent)
 
-  const form = document.createElement('form')
-  form.style.display = 'none'
-  replyToggle.addEventListener('click', () => form.style.display = 'block')
+    const replyToggle = document.createElement('a')
+    replyToggle.setAttribute('href', 'javascript:void(0)')
+    replyToggle.innerText = 'Reply'
+    comment.appendChild(replyToggle)
 
-  const textArea = document.createElement('textarea')
-  textArea.setAttribute('cols', '30')
-  textArea.setAttribute('rows', '10')
-  if (storage === null) {
-    textArea.innerText = data.comments[0].content
-  } else {
-    textArea.innerText = storage.content
-  }
-  form.appendChild(textArea)
-
-  const replyButton = document.createElement('button')
-  replyButton.setAttribute('type', 'submit')
-  replyButton.innerText = 'REPLY'
-  form.appendChild(replyButton)
-
-  comment.appendChild(form)
-
-  form.addEventListener('submit', (event) => {
-    event.preventDefault()
-
-    const inputObj = {
-      content: textArea.value
-    };
-    const input = JSON.stringify(inputObj);
-    localStorage.setItem("testJSON", input);
-
-    commentContent.innerText = textArea.value
-
+    const form = document.createElement('form')
     form.style.display = 'none'
-  })
+    replyToggle.addEventListener('click', () => form.style.display = 'block')
 
-  commentContainer.appendChild(comment)
+    const textArea = document.createElement('textarea')
+    textArea.setAttribute('cols', '30')
+    textArea.setAttribute('rows', '10')
+    if (storage === null) {
+      textArea.innerText = dataComment.content
+    } else {
+      textArea.innerText = storage.content
+    }
+    form.appendChild(textArea)
+
+    const replyButton = document.createElement('button')
+    replyButton.setAttribute('type', 'submit')
+    replyButton.innerText = 'REPLY'
+    form.appendChild(replyButton)
+
+    comment.appendChild(form)
+
+    form.addEventListener('submit', (event) => {
+      event.preventDefault()
+
+      const inputObj = {
+        content: textArea.value
+      }
+      const input = JSON.stringify(inputObj)
+      localStorage.setItem(key, input)
+
+      commentContent.innerText = textArea.value
+
+      form.style.display = 'none'
+    })
+
+    commentContainer.appendChild(comment)
+  }
 }
