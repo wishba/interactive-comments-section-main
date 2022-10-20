1adb9ef667220f2e5772826de9f59d4386aa7825
wishba <wisnubayu34@gmail.com>
2022-10-20 10:36:30 +0700

    todo

--- script.js
+++ script.js
@@ -1,3 +1,41 @@
+// === localStorage format ===
+// key: keyIdFromJSON or keyTimestamp,
+// comments: [
+//   {
+//    content,
+//    createdAt: someTimeAgo,
+//    score: {
+//      totalScore,
+//      whoGiveScore: [username, ...]
+//    },
+//    user: {
+//      image: {
+//        png,
+//        webp,
+//      },
+//    username
+//    },
+//    replies: [keyIdFromJSON or keyTimestamp, ...],
+//   }, ...
+// ]
+
+// === features ===
+// render all data
+// send data
+// send reply data
+// update currentUser data
+// delete currentUser data
+
+// === component ===
+// comment  
+// comment reply  
+// comment currentUser  
+// form send  
+// form reply  
+// form update  
+
+
+
 fetch('data.json')
   .then((response) => response.json())
   .then((data) => appendData(data))
